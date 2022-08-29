aa5405501dd448c6ddda775397677f7cef3bca21
czyt <czyt@w.cn>
2022-08-29 10:34:09 +0800

    update comment

--- secretKeyProvider.go
+++ secretKeyProvider.go
@@ -1,5 +1,7 @@
 package aksk
 
+// SecretKeyProvider provide an interface to get user's secretKey via
+// accessKey (such as db or file)
 type SecretKeyProvider interface {
 	GetSecretKey(accessKey string) (secretKey string, err error)
 }
